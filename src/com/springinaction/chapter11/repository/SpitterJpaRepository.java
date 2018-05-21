e699f79a2e46012cdfdfdc124c214cc8ea93c4a0
MonkGirl <monkgirl2014@gmail.com>
2018-05-21 14:35:07 +0800

    Springframework5.0.6

--- src/com/springinaction/chapter11/repository/SpitterJpaRepository.java
+++ src/com/springinaction/chapter11/repository/SpitterJpaRepository.java
@@ -1,9 +1,11 @@
 package com.springinaction.chapter11.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springinaction.chapter11.entity.Spitter;
 
+@Repository
 public interface SpitterJpaRepository extends JpaRepository<Spitter, Long>{
 	Spitter findByUsername(String username);
 }
